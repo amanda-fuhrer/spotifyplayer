e27943c170b7a44180480cf45189cd483951ce2b
amanda-fuhrer <amandafuhrer@gmail.com>
2024-02-27 19:19:57 -0500

    Add animation play state control to RecordPlayer

--- src/components/RecordPlayer/RecordPlayer.jsx
+++ src/components/RecordPlayer/RecordPlayer.jsx
@@ -1,21 +1,24 @@
+import { useState, useEffect } from "react";
 import "./RecordPlayer.scss";
 import Needle from "../../assets/images/needle.svg";
 
 function RecordPlayer({ currentSong, isPlaying }) {
+  const [animationClass, setAnimationClass] = useState("");
+
+  useEffect(() => {
+    setAnimationClass(isPlaying ? "run" : "pause");
+  }, [isPlaying]);
+
   return (
     <section className="record-player">
-      <div className={`record-player__record ${isPlaying ? 'spin' : ''}`}>
+      <div className={`record-player__record ${animationClass}`}>
         <img
           src={currentSong.album.images[1].url}
           alt="album cover"
           className="record-player__record__cover"
         />
       </div>
-      <img
-          src={Needle}
-          alt="record needle"
-          className="record-player__needle"
-        />
+      <img src={Needle} alt="record needle" className="record-player__needle" />
     </section>
   );
 }
